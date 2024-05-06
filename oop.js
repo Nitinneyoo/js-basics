0017fc402924531a343037bbc0f4b1446158a41e
NeYoO <nitinsinghji12@gmail.com>
2024-05-06 16:07:44 +0530

    new

--- oop.js
+++ oop.js
@@ -21,7 +21,61 @@ function Circle1 (radius) {
         console.log('draw');
     }
 }
-const Another = new circle1(1);
+const Another = new Circle1(1);
 console.log(Another);
 
+
+// value types and reference types 
+
+// Primitive are copied by their value 
+// Objects are copied by their reference
+
+// Abstraction ------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+
+
+// Exercise Stop Watch
+
+function Stopwatch(){
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function (){
+        if (running)
+            throw new Error ('Stopwatch has already started. ');
+
+        running = true ;
+        startTime = new Date();
+    };
+
+    this.stop = function (){
+        if(!running)
+            throw new Error ('Stopwatch is not running. ');
+
+        running = false;
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
+
+    this.reset = function(){
+
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+
+
+    Object.defineProperty (this, 'duration', {
+        get : function() { return duration;}
+    });
+
+}
+
+
+// Prototype And prototypical inheritance
+ //----------------------------------------------------
+ 
 
